66ed9d12f53191d67f6cd812aaef477c29232951
Sergey Kryvets <s.kryvets@gmail.com>
2025-07-19 15:32:27 -0500

    Remove header

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,10 @@
-import {useState} from 'react'
-import {Dialog, DialogPanel} from '@headlessui/react'
-import {XMarkIcon} from '@heroicons/react/24/outline'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMicrophone} from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
-        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-          <div className="fixed inset-0 z-50"/>
-          <DialogPanel
-            className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  alt=""
-                  src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                  className="h-8 w-auto"
-                />
-              </a>
-              <button
-                type="button"
-                onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-400"
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon aria-hidden="true" className="size-6"/>
-              </button>
-            </div>
-          </DialogPanel>
-        </Dialog>
-      </header>
       <main>
         <div className="relative isolate overflow-hidden bg-gray-900 pt-14 pb-16 sm:pb-20 h-screen">
           <img
